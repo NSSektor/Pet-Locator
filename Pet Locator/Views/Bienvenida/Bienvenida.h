214401b2dd6a60c3d9f275d7ed3080f0cbf17760
NSSektor <angel.sektor@gmail.com>
2015-06-23 14:08:32 -0500

    Images Perro

---
+++ Pet Locator/Views/Bienvenida/Bienvenida.h
@@ -0,0 +1,13 @@
+//
+//  Bienvenida.h
+//  Pet Locator
+//
+//  Created by Angel Rivas on 6/23/15.
+//  Copyright (c) 2015 tecnologizame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Bienvenida : UIViewController
+
+@end
---
+++ Pet Locator/Views/Bienvenida/Bienvenida.m
@@ -0,0 +1,37 @@
+//
+//  Bienvenida.m
+//  Pet Locator
+//
+//  Created by Angel Rivas on 6/23/15.
+//  Copyright (c) 2015 tecnologizame. All rights reserved.
+//
+
+#import "Bienvenida.h"
+
+@interface Bienvenida ()
+
+@end
+
+@implementation Bienvenida
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
